bc0e4c13ba1f07b7f35f4cb7c242b5da7f7b555f
dhanmeet-singh <47084570+dhanmeet-singh@users.noreply.github.com>
2020-10-26 13:33:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def hello():
   print('This is a sample python program');
   return 'ok';
+
+def test_hello():
+  assert hello() == 'ok';
   
 if __name__ == '__main__':
-  assert hello() == 'ok';
+  hello();
